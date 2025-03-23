834aa78bc39bf47b31d56c59e851cf2e7f217bef
Sam Roberts <114627670+sjroberts2004@users.noreply.github.com>
2025-03-23 18:34:17 -0500

    Ensure NavBar import matches case for Vercel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar.js';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
